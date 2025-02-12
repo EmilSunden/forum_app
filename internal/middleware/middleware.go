5a8e5d03cbff0b5eb723e285d8b3908020ff31f2
EmilSunden <emsudevcode@gmail.com>
2025-02-12 22:40:41 +0100

    .

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -7,16 +7,17 @@ import (
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Get the token from the Authorization header
+		// Get the token from the cookie
 		cookie, err := r.Cookie("token")
 		if err != nil {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
 
-		// validate the token
+		// assign the value of the cookie which is the token to the token variable
 		token := cookie.Value
 
+		// Validate the token
 		if !auth.ValidateJWT(token) {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
